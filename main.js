5514cb91a39d3fce62dc0e16296050ccd4c1e9d1
RudyMathis <103608310+RudyMathis@users.noreply.github.com>
2022-07-01 18:57:56 -0400

    Check modal works total backers updates plus 1 each time user completes backing money updates based on how much user puts in bug needs to be fixed: money doesnt stay updated based on previous donation

--- main.js
+++ main.js
@@ -1,30 +1,82 @@
 const backThisProject = document.querySelector(".back-this-project");
 const pledgeModal = document.querySelector(".pledge-modal");
 const closeModal = document.querySelector(".close-modal");
-
 const radio = document.querySelectorAll(".radio");
-const enterPledge = document.querySelectorAll(".enter-pledge");
+const continueBtn = document.querySelectorAll(".continue");
+const thankYou = document.querySelector(".thank-you");
+const thankYouBtn = document.querySelector(".thank-you-btn");
+const backerNumber = document.querySelector(".backer-number");
+const pledgeInput = document.querySelectorAll(".pledge-input-amount");
+const moneyBacked = document.querySelector(".money-backed");
+let givenNumber = parseInt(89914);
+
+let count = 5007;
+
 
 backThisProject.addEventListener("click", ()=>{
     pledgeModal.classList.remove("hidden");
-})
+});
+
+thankYouBtn.addEventListener("click", ()=>{
+    resetModal();
+    thankYou.style.display = "none";
+    thankYou.classList.add("hidden");
+    
+    backerNumber.innerHTML = new Intl.NumberFormat().format(++count);
+});
 
 
 closeModal.addEventListener("click", ()=>{
+    resetModal(); 
     pledgeModal.classList.add("hidden");
-})
+});
 
-radio.forEach(function(item){
-    item.addEventListener('click', function(){
+radio.forEach((item)=>{
+    item.addEventListener('click', ()=>{
+        // each article card
         let pledge = item.parentElement.parentElement;
+        // each section pledge
         let check =  item.parentElement.parentElement.lastElementChild;
-        radio.forEach(function(item) {
+        // activates individual cards and removes activate from other cards
+        radio.forEach((item)=> {
+            resetModal(item);
+            });
+        pledge.classList.add('pledge-cyan');
+        check.classList.add('active');
+    }, false);
+});
+
+// closes current modal and opens thank you modal
+continueBtn.forEach((btn)=>{
+    btn.addEventListener("click",()=>{
+        pledgeModal.classList.add("hidden");
+        thankYou.style.display = "flex";
+        thankYou.classList.remove("hidden");
+})});
+
+// function to reset modal
+function resetModal(){
+    radio.forEach((item)=> {
             let pledge = item.parentElement.parentElement;
             let check =  item.parentElement.parentElement.lastElementChild;
-            pledge.classList.remove("pledge-cyan")
-            check.classList.remove('active')
-        })
-        pledge.classList.add('pledge-cyan')
-        check.classList.add('active')
-    }, false)
+            pledge.classList.remove("pledge-cyan");
+            check.classList.remove('active');
+})};
+
+
+pledgeInput.forEach((input)=>{
+    input.addEventListener("input", ()=>{
+
+        moneyBacked.innerHTML = givenNumber;
+        internationalNumberFormat = new Intl.NumberFormat('en-US');
+        moneyBacked.innerHTML = `\$${internationalNumberFormat.format(givenNumber + parseInt(input.value))}`
+
+        if(moneyBacked.innerHTML == "$NaN"){
+
+            moneyBacked.innerHTML = "$89,914"
+        }
+
+    })
 })
+
+
