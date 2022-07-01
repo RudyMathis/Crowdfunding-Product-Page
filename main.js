fcc44292ae4127b72857064c26dc76bd2c180e6e
RudyMathis <103608310+RudyMathis@users.noreply.github.com>
2022-06-30 21:58:03 -0400

    Worked on modal functionality fixed minor deisgn discrepancies

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const backThisProject = document.querySelector(".back-this-project");
 const pledgeModal = document.querySelector(".pledge-modal");
 const closeModal = document.querySelector(".close-modal");
 
+const radio = document.querySelectorAll(".radio");
+const enterPledge = document.querySelectorAll(".enter-pledge");
 
 backThisProject.addEventListener("click", ()=>{
     pledgeModal.classList.remove("hidden");
@@ -10,4 +12,19 @@ backThisProject.addEventListener("click", ()=>{
 
 closeModal.addEventListener("click", ()=>{
     pledgeModal.classList.add("hidden");
+})
+
+radio.forEach(function(item){
+    item.addEventListener('click', function(){
+        let pledge = item.parentElement.parentElement;
+        let check =  item.parentElement.parentElement.lastElementChild;
+        radio.forEach(function(item) {
+            let pledge = item.parentElement.parentElement;
+            let check =  item.parentElement.parentElement.lastElementChild;
+            pledge.classList.remove("pledge-cyan")
+            check.classList.remove('active')
+        })
+        pledge.classList.add('pledge-cyan')
+        check.classList.add('active')
+    }, false)
 })
