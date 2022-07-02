9dd1d432ec8aebcb55cd9cacfe40d986c51d81fe
RudyMathis <103608310+RudyMathis@users.noreply.github.com>
2022-07-02 13:45:57 -0400

    still need to keep inner html persistant added bookmark functionailty refactored  html, css, js

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
+// back this modal prompt
 const backThisProject = document.querySelector(".back-this-project");
-const pledgeModal = document.querySelector(".pledge-modal");
-const closeModal = document.querySelector(".close-modal");
-const radio = document.querySelectorAll(".radio");
-const continueBtn = document.querySelectorAll(".continue");
-const thankYou = document.querySelector(".thank-you");
-const thankYouBtn = document.querySelector(".thank-you-btn");
-const backerNumber = document.querySelector(".backer-number");
-const pledgeInput = document.querySelectorAll(".pledge-input-amount");
-const moneyBacked = document.querySelector(".money-backed");
-let givenNumber = parseInt(89914);
-
-let count = 5007;
-
 
 backThisProject.addEventListener("click", ()=>{
     pledgeModal.classList.remove("hidden");
 });
+
+// thank you prompt
+const thankYou = document.querySelector(".thank-you");
+const thankYouBtn = document.querySelector(".thank-you-btn");
+const backerNumber = document.querySelector(".backer-number");
+let count = 5007;
 
 thankYouBtn.addEventListener("click", ()=>{
     resetModal();
@@ -25,11 +19,17 @@ thankYouBtn.addEventListener("click", ()=>{
     backerNumber.innerHTML = new Intl.NumberFormat().format(++count);
 });
 
+// close modal and reset
+const pledgeModal = document.querySelector(".pledge-modal");
+const closeModal = document.querySelector(".close-modal");
 
 closeModal.addEventListener("click", ()=>{
     resetModal(); 
     pledgeModal.classList.add("hidden");
 });
+
+// radio selected
+const radio = document.querySelectorAll(".radio");
 
 radio.forEach((item)=>{
     item.addEventListener('click', ()=>{
@@ -47,6 +47,8 @@ radio.forEach((item)=>{
 });
 
 // closes current modal and opens thank you modal
+const continueBtn = document.querySelectorAll(".continue");
+
 continueBtn.forEach((btn)=>{
     btn.addEventListener("click",()=>{
         pledgeModal.classList.add("hidden");
@@ -63,20 +65,33 @@ function resetModal(){
             check.classList.remove('active');
 })};
 
+// pledge input
+const pledgeInput = document.querySelectorAll(".pledge-input-amount");
+const moneyBacked = document.querySelector(".money-backed");
 
 pledgeInput.forEach((input)=>{
     input.addEventListener("input", ()=>{
-
-        moneyBacked.innerHTML = givenNumber;
+        let givenNumber = parseInt(89914);
         internationalNumberFormat = new Intl.NumberFormat('en-US');
+
         moneyBacked.innerHTML = `\$${internationalNumberFormat.format(givenNumber + parseInt(input.value))}`
 
-        if(moneyBacked.innerHTML == "$NaN"){
 
+        if(moneyBacked.innerHTML == "$NaN"){
             moneyBacked.innerHTML = "$89,914"
         }
-
+        
     })
 })
 
+// bookmark
+const bookmark = document.querySelector(".bookmark");
+const circle = document.querySelector("circle")
+const path = document.querySelector("path")
 
+bookmark.addEventListener("click", ()=>{
+    
+    circle.classList.toggle("circle");
+    path.classList.toggle("path");
+
+})
